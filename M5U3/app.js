e049729fd00b84bf9ff35d7b3fec6a5006d2cb7b
Agostina Calienno <agos.paz@gmail.com>
2022-07-25 18:02:24 -0300

    #

--- M5U3/app.js
+++ M5U3/app.js
@@ -26,9 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// pool.query('select * from empleados').then(function (resultados) {
-//   console.log(resultados)
-// });
+pool.query('select * from empleados').then(function (resultados) {
+  console.log(resultados)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
