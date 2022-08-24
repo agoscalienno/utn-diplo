0aec99ef2d154e4a5bd02ec9e2e17522880e35db
Agostina Calienno <agos.paz@gmail.com>
2022-08-24 11:53:35 -0300

    front y back funcionales

--- jbd/frontend/src/pages/NovedadesPage.js
+++ jbd/frontend/src/pages/NovedadesPage.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NovedadItem from '../components/novedades/NovedadItem';
 
 const NovedadesPage = (props) => {
+
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+
+        cargarNovedades();
+    }, []);
+
     return (
-<main className="holder">
-        <h2>Mundo Deco</h2>
-        <div className="novedades">
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>Descripcion - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam optio illum molestiae? Assumenda quis,
-            magni repudiandae cupiditate illo, architecto commodi aliquam omnis modi iusto, libero dicta dignissimos vel
-            eum.</p>
-            <hr/>
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>Descripcion - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam optio illum molestiae? Assumenda quis,
-            magni repudiandae cupiditate illo, architecto commodi aliquam omnis modi iusto, libero dicta dignissimos vel
-            eum.</p>
-            <hr/>
-        </div>
-    </main>
-    )
+        <section className="holder">
+            <h2>Novedades</h2>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                novedades.map(item => <NovedadItem key={item.id}
+                    title={item.titulo} subtitle={item.copete}
+                    imagen={item.imagen} body={item.cuerpo} />)
+            )}
+
+        </section>
+    );
+
 }
 
-export default NovedadesPage
+export default NovedadesPage;
+
+// return (
+//     <main className="holder">
+//         <h2>Mundo Deco</h2>
+//         <div className="novedades">
+//             <h3>Titulo</h3>
+//             <h4>Subtitulo</h4>
+//             <p>Descripcion - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam optio illum molestiae? Assumenda quis,
+//                 magni repudiandae cupiditate illo, architecto commodi aliquam omnis modi iusto, libero dicta dignissimos vel
+//                 eum.</p>
+//             <hr />
+//             <h3>Titulo</h3>
+//             <h4>Subtitulo</h4>
+//             <p>Descripcion - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam optio illum molestiae? Assumenda quis,
+//                 magni repudiandae cupiditate illo, architecto commodi aliquam omnis modi iusto, libero dicta dignissimos vel
+//                 eum.</p>
+//             <hr />
+//         </div>
+//     </main>
+// )
+// }
+
+// export default NovedadesPage
